c33e51d609c6232fdb36b92a49c0627ae6b3ede0
Rewaly <haba0818@icloud.com>
2025-10-30 15:32:56 +0900

    haba

---
+++ src/k24120_lecture05_01.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+from my_module.K24120.lecture05_camera_image_capture import MyVideoCapture
+
+def lecture05_01():
+
+    # カメラキャプチャ実行
+    app = MyVideoCapture()
+    app.run()
+
+    # 撮影した画像を取得
+    capture_img = app.get_img()
+    if capture_omg is None:
+        print("画像が取得できませんでした")
+        return
+
+    # 画像をローカル変数に保存
+    google_img : cv2.Mat = cv2.imread('images/google.png')
+    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
+    # capture_img : cv2.Mat = "implement me"
+
+    g_hight, g_width, g_channel = google_img.shape
+    c_hight, c_width, c_channel = capture_img.shape
+    print("google_img:", google_img.shape)
+    print("google_img:", capture_img.shape)
+
+    for x in range(g_width):
+        for y in range(g_hight):
+            g, b, r = google_img[y, x]
+            # もし白色(255,255,255)だったら置き換える
+            if (b, g, r) == (255, 255, 255):
+                google_img[y, x] = capture_img[y % c_hight, x % c_width]
+
+    # 書き込み処理
+    output_filepath = 'output_imgages/lecture05_01_output.png'
+    cv2.imwrite(output_flilepath, google_img)
+    ptint(f"出力完了:"{output_fileppath})
